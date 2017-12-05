366baaf2295163ee99fe6a42b4526b49b759f241
Juan Dematei <juandematei@gmail.com>
2017-12-05 15:01:47 -0300

    Added scroll-top btn

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-//Scroll-to-top
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+(function() {
+
+    $(document).scroll(function() {
+        var y = $('html').scrollTop();
+        if (y > 500) {
+          $('#to-top').fadeIn();
+        } else {
+          $('#to-top').fadeOut();
+        }
+      });
+
+    $(document).ready(function() {
+        $('#to-top').click(function() {
+            $('html').animate({scrollTop: 0}, 500);
+        });
+    });
+
+})();
