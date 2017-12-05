bf40104bec2ce3baed16c3a4372ce51f7ca30517
Juan Dematei <juandematei@gmail.com>
2017-12-05 17:09:30 -0300

    Moved "instafeed" to external js

--- js/app.js
+++ js/app.js
@@ -15,4 +15,15 @@
         });
     });
 
+    $(document).ready(function() {
+        var feed = new Instafeed({
+            get: 'user',
+            userId: '2263540523',
+            accessToken: '2263540523.1677ed0.109f5423cfb14c7ebe5a7e91c362ee2f',
+            limit: '10',
+            template: '<a href="{{link}}" target="_blank"><img class="rounded m-3" src="{{image}}" /></a>'
+        });
+        feed.run();
+    });
+
 })();
